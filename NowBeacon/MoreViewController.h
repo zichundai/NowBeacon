7230eed96b008e5f19b35c201b16e3959358f104
Daijian <daijian@cellteltech.cn>
2015-05-28 23:07:04 +0800

    add more page

---
+++ NowBeacon/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  NowBeacon
+//
+//  Created by carvin on 15/5/28.
+//  Copyright (c) 2015年 SZMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UIViewController
+
+@end
