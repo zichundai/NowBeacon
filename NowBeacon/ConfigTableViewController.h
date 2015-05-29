bfda28f31d925723542f32bd2c11467df3676194
Daijian <daijian@cellteltech.cn>
2015-05-29 15:37:05 +0800

    完成三个界面概要

---
+++ NowBeacon/ConfigTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigTableViewController.h
+//  NowBeacon
+//
+//  Created by carvin on 15/5/29.
+//  Copyright (c) 2015年 SZMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigTableViewController : UITableViewController
+
+@end
--- NowBeacon/FirstViewController.m
+++ NowBeacon/FirstViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //[self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background.png" ] ]];
+    //[self.view setAlpha:1];
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:self.view.bounds];
+    imageView.image = [UIImage imageNamed:@"background.png"];
+    imageView.alpha = 1;
+    [self.view addSubview:imageView];
+
 }
 
 - (void)didReceiveMemoryWarning {
